a8b60ba950e7d6bf369ce5b9e86ee840ee8fc02a
susumu.yata <susumu.yata@gmail.com>
2012-12-14 17:40:14 +0900

    Add a comman line tool "grnxx".
    Only "grnxx --version" is available.

---
+++ src/grnxx.cpp
@@ -0,0 +1,48 @@
+// FIXME
+#include <getopt.h>
+
+#include <iostream>
+
+#include "grnxx.hpp"
+
+namespace {
+
+void print_version() {
+  std::cout << "grnxx " << grnxx::Grnxx::version() << std::endl;
+}
+
+void print_usage() {
+  std::cout << "Usage: grnxx [options...]\n\n"
+            << "Options:\n"
+            << "  -h, --help:     show usage\n"
+            << "  -v, --version:  show grnxx version"
+            << std::endl;
+}
+
+}  // namespace
+
+int main(int argc, char *argv[]) {
+  const struct option long_options[] = {
+    { "help", 0, nullptr, 'h' },
+    { "version", 0, nullptr, 'v' },
+    { nullptr, 0, nullptr, 0 }
+  };
+  int value;
+  while ((value = ::getopt_long(argc, argv, "hv",
+                                long_options, nullptr)) != -1) {
+    switch (value) {
+      case 'h': {
+        print_usage();
+        break;
+      }
+      case 'v': {
+        print_version();
+        return 0;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+  return 0;
+}
