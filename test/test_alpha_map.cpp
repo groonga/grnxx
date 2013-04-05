1a20831368167b0bfc41c206e29990111a752668
susumu.yata <susumu.yata@gmail.com>
2013-04-05 10:27:31 +0900

    Add tests for grnxx::alpha::Map.

--- test/test_alpha_map.cpp
+++ test/test_alpha_map.cpp
@@ -42,6 +42,8 @@ void compare_maps(const std::unique_ptr<grnxx::alpha::Map<T>> &map,
 
 template <typename T>
 void test_map() {
+  GRNXX_NOTICE() << __PRETTY_FUNCTION__;
+
   std::mt19937_64 rng;
 
   grnxx::io::Pool pool;
@@ -66,8 +68,72 @@ void test_map() {
     std::int64_t stored_key_id;
     assert(map->insert(key, &stored_key_id) == is_new);
     assert(stored_key_id == key_id);
+    assert(!map->insert(key, &stored_key_id));
 
-    compare_maps(map, hash_map);
+    T stored_key;
+    assert(map->get(key_id, &stored_key));
+    assert(stored_key == key);
+
+    assert(map->search(key, &stored_key_id));
+    assert(stored_key_id == key_id);
+  }
+  compare_maps(map, hash_map);
+
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    assert(map->unset(it->second));
+    assert(!map->unset(it->second));
+  }
+
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    assert(map->insert(it->first));
+  }
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    assert(map->remove(it->first));
+    assert(!map->remove(it->first));
+  }
+
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    assert(map->insert(it->first));
+  }
+  map->truncate();
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    assert(!map->get(it->second));
+  }
+
+  map->truncate();
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    assert(map->insert(it->first, &it->second));
+  }
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    auto old_it = it;
+    auto new_it = ++it;
+    assert(map->unset(new_it->second));
+    assert(map->reset(old_it->second, new_it->first));
+
+    T key;
+    assert(map->get(old_it->second, &key));
+    assert(key == new_it->first);
+    std::int64_t key_id;
+    assert(map->search(key, &key_id));
+    assert(key_id == old_it->second);
+  }
+
+  map->truncate();
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    assert(map->insert(it->first, &it->second));
+  }
+  for (auto it = hash_map.begin(); it != hash_map.end(); ++it) {
+    auto old_it = it;
+    auto new_it = ++it;
+    assert(map->remove(new_it->first));
+    assert(map->update(old_it->first, new_it->first));
+
+    T key;
+    assert(map->get(old_it->second, &key));
+    assert(key == new_it->first);
+    std::int64_t key_id;
+    assert(map->search(key, &key_id));
+    assert(key_id == old_it->second);
   }
 }
 
