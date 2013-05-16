7ab451a8b5b7793bbf08a916f1bb602849574470
susumu.yata <susumu.yata@gmail.com>
2013-05-16 20:59:37 +0900

    Add missing #includes.

--- test/test_array.cpp
+++ test/test_array.cpp
@@ -16,6 +16,7 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #include <cassert>
+#include <memory>
 
 #include "grnxx/array.hpp"
 #include "grnxx/logger.hpp"
