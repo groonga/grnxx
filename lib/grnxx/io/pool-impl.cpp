e34064ce437a40b4837fd6c323ec27e6a5b49d38
susumu.yata <susumu.yata@gmail.com>
2013-05-28 10:00:49 +0900

    Fix a wrong header path.

--- lib/grnxx/io/pool-impl.cpp
+++ lib/grnxx/io/pool-impl.cpp
@@ -15,7 +15,7 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
-#include "grnxx/io/pool-impl.hpp"
+#include "grnxx/pool-impl.hpp"
 
 #include <vector>
 
