cbb56e2c73940b09b768c3fb757b21a101c18ea4
susumu.yata <susumu.yata@gmail.com>
2013-05-28 19:53:01 +0900

    Update gnrxx::map::ArrayMap to use grnxx::map::Bitmap.

--- lib/grnxx/map/array_map.hpp
+++ lib/grnxx/map/array_map.hpp
@@ -21,8 +21,8 @@
 #include "grnxx/features.hpp"
 
 #include "grnxx/array.hpp"
-#include "grnxx/bit_array.hpp"
 #include "grnxx/map.hpp"
+#include "grnxx/map/bitmap.hpp"
 #include "grnxx/types.hpp"
 
 namespace grnxx {
@@ -71,7 +71,7 @@ class ArrayMap : public Map<T> {
   Storage *storage_;
   uint32_t storage_node_id_;
   ArrayMapHeader *header_;
-  BitArray<> bitmap_;
+  Bitmap<T> bitmap_;
   Array<T> keys_;
 
   bool create_map(Storage *storage, uint32_t storage_node_id,
