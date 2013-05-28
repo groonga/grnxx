98b32c1740b126a0e61f9f266e6d5688970fc7a1
susumu.yata <susumu.yata@gmail.com>
2013-05-28 17:57:10 +0900

    Remove specialization for bit array (grnxx::Array).

--- test/test_array.cpp
+++ test/test_array.cpp
@@ -79,65 +79,6 @@ void test_array() {
   }
 }
 
-void test_bit_array() {
-  constexpr std::uint64_t PAGE_SIZE            = 64;
-  constexpr std::uint64_t TABLE_SIZE           = 32;
-  constexpr std::uint64_t SECONDARY_TABLE_SIZE = 16;
-
-  GRNXX_NOTICE() << __PRETTY_FUNCTION__;
-
-  // Create an anonymous Storage.
-  std::unique_ptr<grnxx::Storage> storage(grnxx::Storage::create(nullptr));
-  grnxx::Array<bool, PAGE_SIZE, TABLE_SIZE, SECONDARY_TABLE_SIZE> array;
-  uint32_t storage_node_id;
-
-  // Create an Array and test its member functions.
-  assert(array.create(storage.get(), grnxx::STORAGE_ROOT_NODE_ID));
-  assert(array);
-  assert(array.page_size() == PAGE_SIZE);
-  assert(array.table_size() == TABLE_SIZE);
-  assert(array.secondary_table_size() == SECONDARY_TABLE_SIZE);
-  assert(array.size() == (PAGE_SIZE * TABLE_SIZE * SECONDARY_TABLE_SIZE));
-  storage_node_id = array.storage_node_id();
-
-  for (std::uint64_t i = 0; i < array.size(); ++i) {
-    const bool value = (i % 3) != 0;
-    assert(array.set(i, value));
-  }
-  for (std::uint64_t i = 0; i < array.size(); ++i) {
-    const bool expected_value = (i % 3) != 0;
-    bool value;
-    assert(array.get(i, &value));
-    assert(value == expected_value);
-  }
-  for (std::uint64_t i = 0; i < array.size(); ++i) {
-    const bool expected_value = (i % 3) != 0;
-    assert(array[i] == expected_value);
-  }
-  for (std::uint64_t i = 0; i < (array.size() / array.unit_size()); ++i) {
-    assert(array.get_unit(i));
-  }
-  for (std::uint64_t i = 0; i < (array.size() / array.page_size()); ++i) {
-    assert(array.get_page(i));
-  }
-
-  // Open the Array.
-  assert(array.open(storage.get(), storage_node_id));
-  for (std::uint64_t i = 0; i < array.size(); ++i) {
-    const bool expected_value = (i % 3) != 0;
-    bool value;
-    assert(array.get(i, &value));
-    assert(value == expected_value);
-  }
-
-  // Create an Array with default value.
-  assert(array.create(storage.get(), grnxx::STORAGE_ROOT_NODE_ID, true));
-  assert(array);
-  for (std::uint64_t i = 0; i < array.size(); ++i) {
-    assert(array[i]);
-  }
-}
-
 }  // namesapce
 
 int main() {
@@ -145,10 +86,9 @@ int main() {
                            grnxx::LOGGER_ENABLE_COUT);
   grnxx::Logger::set_max_level(grnxx::NOTICE_LOGGER);
 
-  test_array<64,  1,  1>();
-  test_array<64, 32,  1>();
-  test_array<64, 32, 16>();
-  test_bit_array();
+  test_array<256,  1,  1>();
+  test_array<256, 64,  1>();
+  test_array<256, 64, 16>();
 
   return 0;
 }
