692f4b640c2bb4d072b74120cc20f74bed2cd922
susumu.yata <susumu.yata@gmail.com>
2014-06-19 13:35:54 +0900

    Rename Pipeline::run() to Pipeline::flush().

--- new-interface/pipeline.hpp
+++ new-interface/pipeline.hpp
@@ -10,18 +10,17 @@ class Pipeline {
   Pipeline();
   virtual ~Pipeline();
 
-  // 所属するテーブルを取得する．
-  virtual Table *table() const = 0;
-
   // パイプラインを通してレコードの一覧を取得する．
   // 成功すれば true を返す．
   // 失敗したときは *error にその内容を格納し， false を返す．
   //
+  // パイプラインは使い捨てなので，二回目の呼び出しは失敗する．
+  //
   // 失敗する状況としては，以下のようなものが挙げられる．
   // - 演算において例外が発生する．
   // - リソースを確保できない．
-  virtual bool run(Error *error,
-                   RecordSet *record_set);
+  virtual bool flush(Error *error,
+                     RecordSet *record_set);
 };
 
 }  // namespace grnxx
