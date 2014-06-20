90ecdb4538dec59c14b186b56e7ec94c6a5007ae
susumu.yata <susumu.yata@gmail.com>
2014-06-20 16:54:43 +0900

    Remove member functions from grnxx::RecordSet.

--- new-interface/record-set.hpp
+++ new-interface/record-set.hpp
@@ -31,50 +31,6 @@ class RecordSet {
   // スコアを取得する．
   // 不正なレコード ID を指定したときの動作は未定義である．
   double get_score(int64_t i) const;
-
-  // 一部を取得する．
-  // 不正な範囲を指定したときの動作は未定義である．
-  RecordSet subset(int64_t offset, int64_t count) const;
-
-  // スコアを正規化する．
-  // 成功すれば true を返す．
-  // 失敗したときは *error にその内容を格納し， false を返す．
-  //
-  // TODO: 具体的な正規化方法を決める．
-  //       最大値ですべてのスコアを除算するというのが簡単そうである．
-  //       正規化後の最大値を指定できると便利かもしれない．
-  //       ほかにも何か必要な正規化があるかどうか．
-  //
-  // 失敗する状況としては，以下のようなものが挙げられる．
-  // - 最大値が 0.0, negative, infinity のいずれかである．
-  bool normalize(Error *error, const NormalizeOptions &options);
-
-  // TODO: Sorter を使うより RecordSet::sort() の方が良い？
-
-  // 整列する．
-  // 成功すれば true を返す．
-  // 失敗したときは *error にその内容を格納し， false を返す．
-  //
-  // TODO: 整列条件の指定方法を決める．
-  //
-  // 失敗する状況としては，以下のようなものが挙げられる．
-  // - リソースが足りない．
-  // - 演算において例外が発生する．
-  bool sort(Error *error, const SortConditions &conditions);
-
-  // TODO: Grouper を使うより RecordSet::group() の方が良い？
-
-  // グループ化する．
-  // 成功すれば true を返す．
-  // 失敗したときは *error にその内容を格納し， false を返す．
-  //
-  // 失敗する状況としては，以下のようなものが挙げられる．
-  // - オプションが不正である．
-  // - リソースが確保できない．
-  bool group(Error *error,
-             GroupSet *group_set,
-             const Expression &expression,
-             GroupOptions &options) const;
 };
 
 }  // namespace grnxx
