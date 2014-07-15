b08ac18e56c96e527cb21c6dcea936867e7cfa1b
susumu.yata <susumu.yata@gmail.com>
2014-07-15 18:56:12 +0900

    Add Cursor::read_all().

--- include/grnxx/cursor.hpp
+++ include/grnxx/cursor.hpp
@@ -19,12 +19,21 @@ class Cursor {
   // Reads at most "max_count" records and stores the records into
   // "*record_set".
   //
-  // On success, returns the number of records read on success.
+  // On success, returns the number of records read.
   // On failure, returns -1 and stores error information into "*error" if
   // "error" != nullptr.
   virtual Int read(Error *error,
                    Int max_count,
                    RecordSet *record_set) = 0;
+
+  // Read all the remaining records.
+  //
+  // On success, returns the number of records read.
+  // On failure, returns -1 and stores error information into "*error" if
+  // "error" != nullptr.
+  virtual Int read_all(Error *error, RecordSet *record_set) {
+    return read(error, numeric_limits<Int>::max(), record_set);
+  }
 
  protected:
   const Table *table_;
