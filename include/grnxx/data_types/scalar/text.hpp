e682d2fceb31a19c5024affb856bf30c7e9919cb
susumu.yata <susumu.yata@gmail.com>
2014-11-10 11:38:24 +0900

    Remove comments. (#92)

--- include/grnxx/data_types/scalar/text.hpp
+++ include/grnxx/data_types/scalar/text.hpp
@@ -69,13 +69,6 @@ class Text {
     int data_result = std::memcmp(data_, rhs.data_, min_size);
     return (data_result < 0) ? Bool(true) :
            ((data_result == 0) ? has_less_size : Bool(false));
-
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    size_t min_size = size_ < rhs.size_ ? size_ : rhs.size_;
-//    int result = std::memcmp(data_, rhs.data_, min_size);
-//    return Bool((result < 0) || ((result == 0) && (size_ < rhs.size_)));
   }
   Bool operator>(const Text &rhs) const {
     return rhs < *this;
@@ -90,13 +83,6 @@ class Text {
     int data_result = std::memcmp(data_, rhs.data_, min_size);
     return (data_result < 0) ? Bool(true) :
            ((data_result == 0) ? has_less_or_equal_size : Bool(false));
-
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    size_t min_size = size_ < rhs.size_ ? size_ : rhs.size_;
-//    int result = std::memcmp(data_, rhs.data_, min_size);
-//    return Bool((result < 0) || ((result == 0) && (size_ <= rhs.size_)));
   }
   Bool operator>=(const Text &rhs) const {
     return rhs <= *this;
@@ -108,14 +94,6 @@ class Text {
       return Bool(std::memcmp(data_, rhs.data_, rhs.size_.value()) == 0);
     }
     return has_greater_or_equal_size;
-
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    if (size_ < rhs.size_) {
-//      return Bool(false);
-//    }
-//    return Bool(std::memcmp(data_, rhs.data_, rhs.size_) == 0);
   }
   Bool ends_with(const Text &rhs) const {
     Bool has_greater_or_equal_size = (size_ >= rhs.size_);
@@ -124,15 +102,6 @@ class Text {
                               rhs.data_, rhs.size_.value()) == 0);
     }
     return has_greater_or_equal_size;
-
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    if (size_ < rhs.size_) {
-//      return Bool(false);
-//    }
-//    return Bool(std::memcmp(data_ + size_ - rhs.size_,
-//                            rhs.data_, rhs.size_) == 0);
   }
 
   static constexpr DataType type() {
@@ -150,122 +119,6 @@ class Text {
   const char *data_;
   Int size_;
 };
-
-//// Reference to a byte string.
-//class Text {
-// public:
-//  Text() = default;
-//  ~Text() = default;
-
-//  constexpr Text(const Text &) = default;
-//  Text &operator=(const Text &) = default;
-
-//  explicit Text(const char *str)
-//      : data_(str),
-//        size_(str ? std::strlen(str) : na_size()) {}
-//  constexpr Text(const char *data, size_t size) : data_(data), size_(size) {}
-//  explicit constexpr Text(NA) : data_(na_data()), size_(na_size()) {}
-
-//  const char &operator[](size_t i) const {
-//    return data_[i];
-//  }
-//  constexpr const char *data() const {
-//    return data_;
-//  }
-//  constexpr size_t size() const {
-//    return size_;
-//  }
-
-//  constexpr bool is_empty() const {
-//    return !is_na() && (size_ == 0);
-//  }
-//  constexpr bool is_na() const {
-//    return data_ == na_data();
-//  }
-
-//  Bool operator==(const Text &rhs) const {
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    if (size_ != rhs.size_) {
-//      return Bool(false);
-//    }
-//    return Bool(std::memcmp(data_, rhs.data_, size_) == 0);
-//  }
-//  Bool operator!=(const Text &rhs) const {
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    if (size_ != rhs.size_) {
-//      return Bool(true);
-//    }
-//    return Bool(std::memcmp(data_, rhs.data_, size_) != 0);
-//  }
-//  Bool operator<(const Text &rhs) const {
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    size_t min_size = size_ < rhs.size_ ? size_ : rhs.size_;
-//    int result = std::memcmp(data_, rhs.data_, min_size);
-//    return Bool((result < 0) || ((result == 0) && (size_ < rhs.size_)));
-//  }
-//  Bool operator>(const Text &rhs) const {
-//    return rhs < *this;
-//  }
-//  Bool operator<=(const Text &rhs) const {
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    size_t min_size = size_ < rhs.size_ ? size_ : rhs.size_;
-//    int result = std::memcmp(data_, rhs.data_, min_size);
-//    return Bool((result < 0) || ((result == 0) && (size_ <= rhs.size_)));
-//  }
-//  Bool operator>=(const Text &rhs) const {
-//    return rhs <= *this;
-//  }
-
-//  Bool starts_with(const Text &rhs) const {
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    if (size_ < rhs.size_) {
-//      return Bool(false);
-//    }
-//    return Bool(std::memcmp(data_, rhs.data_, rhs.size_) == 0);
-//  }
-//  Bool ends_with(const Text &rhs) const {
-//    if (is_na() || rhs.is_na()) {
-//      return Bool::na();
-//    }
-//    if (size_ < rhs.size_) {
-//      return Bool(false);
-//    }
-//    return Bool(std::memcmp(data_ + size_ - rhs.size_,
-//                            rhs.data_, rhs.size_) == 0);
-//  }
-
-//  static constexpr DataType type() {
-//    return TEXT_DATA;
-//  }
-
-//  static constexpr Text empty() {
-//    return Text("", 0);
-//  }
-//  static constexpr Text na() {
-//    return Text(NA());
-//  }
-
-//  static constexpr const char *na_data() {
-//    return nullptr;
-//  }
-//  static constexpr size_t na_size() {
-//    return 0;
-//  }
-
-// private:
-//  const char *data_;
-//  size_t size_;
-//};
 
 }  // namespace grnxx
 
